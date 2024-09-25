486982b18f9edc336eda2e76cfe0c7d3b181af96
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2024-09-26 00:02:17 +0530

    Route search function implemented

--- frontend/screens/track-bus/trackbus.screen.tsx
+++ frontend/screens/track-bus/trackbus.screen.tsx
@@ -3,6 +3,7 @@ import { WebView } from 'react-native-webview';
 import * as Location from 'expo-location';
 import { Linking, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { SearchContext } from '../../contexts/SearchContext';
 
 interface LocationCoords {
@@ -10,14 +11,23 @@ interface LocationCoords {
   longitude: number;
 }
 
+interface SosModalProps {
+  setIsSosOn: (isOn: boolean) => void;
+}
+
 const TrackbusScreen = () => {
   const [location, setLocation] = useState<LocationCoords | null>(null);
   const [userLocationIsOn, setUserLocationOn] = useState(false);
   const [markersScript, setMarkersScript] = useState('');
   const [leafletHTML, setLeafletHTML] = useState('');
-  const { isSearchActive } = useContext(SearchContext);
+  const { isSearchActive, setIsSearchActive } = useContext(SearchContext);
   const [isSosOn, setIsSosOn] = useState(false);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [isBusClick, setIsBusClick] = useState(false);
+  const [startLocation, SetStartLocation] = useState('');
+  const [destinationLocation, SetDestinationLocation] = useState('');
+  const [isSearchResultActive, SetIsSearchResultActive] = useState(false);
+  const [searchResult, setSearchResult] = useState('');
   
 
   useEffect(() => {
@@ -52,6 +62,89 @@ const TrackbusScreen = () => {
       setMarkersScript(``);
     }
   }
+
+  // const clickHandler = () =>{
+  //   setIsBusClick(!isBusClick);
+  // }   
+
+  const searchHandler = () =>{
+    setUserLocationOn(false);
+    SetIsSearchResultActive(true);
+    setSearchResult(`
+      let routingControl;
+
+      const updateRoute = (startLatLng, destinationLatLng) => {
+        if (routingControl) {
+          mymap.removeControl(routingControl);
+        }
+
+        routingControl = L.Routing.control({
+          waypoints: [
+            startLatLng,
+            destinationLatLng
+          ],
+          routeWhileDragging: true,
+          lineOptions: {
+            styles: [
+              { color: '#152bf4', opacity: 1, weight: 8 },
+              { color: '#3B6DE7', opacity: 1, weight: 4 }
+           ],
+          },
+          show: false,
+          formatter: new L.Routing.Formatter({
+            language: "en",
+            units: "metric",
+            roundingSensitivity: 1,
+            formatInstruction: function () {
+              return "";
+            }, 
+          }),
+        }).addTo(mymap);
+      };
+
+      const geocodeLocation = (location) => {
+        return new Promise((resolve) => {
+          L.Control.Geocoder.nominatim().geocode(location, function(results) {
+            if (results.length > 0) {
+              const latLng = results[0].center;
+              resolve(latLng);
+            } else {
+              console.log('No results found');
+              resolve(null);
+            }
+          });
+        });
+      };
+
+      const fetchAndDisplayRoute = async () => {
+        const startLatLng = await geocodeLocation("${startLocation}");
+        const destinationLatLng = await geocodeLocation("${destinationLocation}");
+
+        console.log(startLatLng, destinationLatLng);  // Corrected typo
+
+        if (startLatLng && destinationLatLng) {
+          updateRoute(startLatLng, destinationLatLng);
+        } else {
+          console.log('Could not get coordinates for routing.');
+        }
+      };
+
+      fetchAndDisplayRoute();
+    `);
+    setIsSearchActive(false);
+  }   
+
+  // const mark=`
+  //       const userLocation = [6.934101, 79.859634];
+  //       const marker = L.marker(userLocation, { icon: userLocationIcon }).addTo(mymap).bindPopup('Your Location');
+  //       mymap.setView(userLocation, 12);
+
+  //      marker.on('click', function() {
+  //       window.ReactNativeWebView.postMessage('markerClicked');
+  //     });
+  //     `
+
+   
 
   useEffect(() => {
     // if (!location) return;
@@ -106,58 +199,24 @@ const TrackbusScreen = () => {
                   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
               }).addTo(mymap);
 
-              let routingControl;
-
-              const r = (location) => {
-                  g(location).then(latLng => {
-                      if (latLng) {
-                          if (routingControl) {
-                              mymap.removeControl(routingControl);
-                          }
-
-                          routingControl = L.Routing.control({
-                              waypoints: [
-                                  L.latLng(6.747721, 80.501112), // Starting point
-                                  latLng // Geocoded destination
-                              ],
-                              routeWhileDragging: true,
-                              lineOptions: {
-                                  styles: [{ color: '#3B6DE7', opacity: 0.8, weight: 5 }] // Blue route color
-                              }
-                          }).addTo(mymap);
-                      } else {
-                          console.log('Could not get coordinates for routing.');
-                      }
-                  });
+              if(${userLocationIsOn}){
+                  const userLocation = [${location?.latitude}, ${location?.longitude}];
+                  L.marker(userLocation, { icon: userLocationIcon }).addTo(mymap).bindPopup('Your Location');
+                  mymap.setView(userLocation, 12);
               }
 
-              const g = (location) => {
-                  return new Promise((resolve) => {
-                      L.Control.Geocoder.nominatim().geocode(location, function(results) {
-                          if (results.length > 0) {
-                              const latLng = results[0].center;
-                              resolve(latLng);
-                          } else {
-                              console.log('No results found');
-                              resolve(null);
-                          }
-                      });
-                  });
+              if(${isSearchResultActive}){
+                ${searchResult}
               }
-
-              ${markersScript}
+ 
             </script>
         </body>
       </html>
     `;
 
     setLeafletHTML((prevHTML) => (prevHTML !== mapHTML ? mapHTML : prevHTML));
-  }, [location, userLocationIsOn, markersScript]);
 
-  const handleCall = () => {
-    const phoneNumber = '119';
-    Linking.openURL(`tel:${phoneNumber}`);
-  };
+  }, [location, userLocationIsOn, markersScript, searchResult, isSearchResultActive]);
   
   return (
     <View className=' h-full relative'>
@@ -165,32 +224,74 @@ const TrackbusScreen = () => {
         originWhitelist={['*']}
         source={{ html: leafletHTML }}
         style={{ flex: 1 }}
+        // onMessage={(event) => {
+        //   if (event.nativeEvent.data === 'markerClicked') {
+        //     clickHandler(); // Call your click handler when the marker is clicked
+        //   }
+        // }}
       />
-      <View className=' absolute bottom-3 right-2 flex justify-center items-center'>
-        <TouchableOpacity onPress={markUserLocation} className=' bg-white flex-1 items-center justify-center h-[35px] w-[35px] rounded-full mb-4'>
-          <Ionicons name="locate" size={28} color="#3B6DE7" />
+      {!isSearchResultActive && (
+        <View className=' absolute bottom-3 right-2 flex justify-center items-center'>
+          <TouchableOpacity onPress={markUserLocation} className=' bg-white flex-1 items-center justify-center h-[35px] w-[35px] rounded-full mb-4'>
+            <Ionicons name="locate" size={28} color="#3B6DE7" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=> setIsSosOn(true)} className=' bg-red-600 flex-1 items-center justify-center h-[50px] w-[50px] rounded-full'>
+            <Text className=' text-white font-bold text-lg'>SOS</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      {isSearchResultActive && (
+        <TouchableOpacity onPress={()=>SetIsSearchResultActive(false)} className=' absolute top-12 right-2 flex justify-center items-center bg-swhite p-1 rounded-full shadow-xl shadow-black'>
+          <Ionicons name="close-sharp" size={24} color="black" />
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=> setIsSosOn(true)} className=' bg-red-600 flex-1 items-center justify-center h-[50px] w-[50px] rounded-full'>
-          <Text className=' text-white font-bold text-lg'>SOS</Text>
-        </TouchableOpacity>
-      </View>
+      )}
+
+      {isSearchResultActive && (
+        <View className=' w-full absolute bottom-4 left-0 flex justify-center items-center'>
+          <TouchableOpacity className=' bg-primary h-16 w-11/12 rounded-lg flex flex-row justify-center items-center'>
+            <Text className=' text-center font-bold text-white text-2xl mr-5'>{startLocation} - {destinationLocation}</Text>
+            <FontAwesome6 name="route" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+      )}
 
       {/* search box */}
       {isSearchActive && (
-        <View className=' absolute -top-5 left-0 bg-Secondary flex flex-row justify-between pl-5 py-7 rounded-b-[20px]'>
-          <View className=' w-[85%]'>
-            <TextInput className=' bg-swhite mb-5 h-10 rounded-md'></TextInput>
-            <TextInput className=' bg-swhite h-10 rounded-md'></TextInput>
+        <View className='absolute -top-5 left-0 bg-Secondary pl-5 py-7 rounded-b-[20px] w-full'>
+          <View className='  flex flex-row justify-between '>
+            <View className=' w-[85%]'>
+              <TextInput className=' bg-swhite mb-5 h-10 rounded-md placeholder:pl-3' onChangeText={(text) => SetStartLocation(text)} value={startLocation} placeholder='Enter start location'></TextInput>
+              <TextInput className=' bg-swhite mb-5 h-10 rounded-md placeholder:pl-3' onChangeText={(text) => SetDestinationLocation(text)} value={destinationLocation} placeholder='Enter destination'></TextInput>
+            </View>
+            <TouchableOpacity className=' flex-1 justify-center items-center'>
+            <Ionicons name="repeat" size={30} color="white" />
+            </TouchableOpacity>
           </View>
-          <TouchableOpacity className=' flex-1 justify-center items-center'>
-          <Ionicons name="repeat" size={30} color="white" />
+          <TouchableOpacity onPress={()=>searchHandler()} className=' bg-primary w-[85%] rounded-md h-10 flex justify-center items-center'>
+            <Text className=' font-bold text-white'>Search Route</Text>
           </TouchableOpacity>
         </View>
       )}
 
       {/* sos box */}
       {isSosOn &&(
-        <View className=' absolute bottom-0 left-0 w-full p-6 rounded-t-3xl bg-swhite '>
+         <SosModal setIsSosOn={setIsSosOn}/>
+      )}
+      {isBusClick&& <Text>clicked</Text>}
+    </View>
+  )
+}
+
+
+const SosModal: React.FC<SosModalProps> = ({ setIsSosOn }) => {
+  const handleCall = () => {
+    const phoneNumber = '119';
+    Linking.openURL(`tel:${phoneNumber}`);
+  };
+
+  return (
+    <View className=' absolute bottom-0 left-0 w-full p-6 rounded-t-3xl bg-swhite '>
           <TouchableOpacity onPressIn={()=>setIsSosOn(false)} className=' absolute -top-5 left-[48%]'>
             <Ionicons name="remove" size={70} color="#A6A6A6" />
           </TouchableOpacity>
@@ -220,10 +321,8 @@ const TrackbusScreen = () => {
               <Text className=' text-base font-semibold text-white ml-2'>Call 119</Text>
             </TouchableOpacity>
           </View>
-        </View> 
-      )}
-    </View>
-  )
-}
+        </View>
+  );
+};
 
 export default TrackbusScreen
