6406f38d7ac1ff12c8f63f30af55c3521042ec2f
IT22343970 <it22343970@my.sliit.lk>
2024-10-09 17:40:56 +0530

    created branch

--- driver/.expo/types/router.d.ts
+++ driver/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(routes)/login` | `/(routes)\earning\` | `/(tabs)` | `/(tabs)/` | `/(tabs)/bookings` | `/(tabs)/profile` | `/(tabs)/schedule` | `/(tabs)/track-bus` | `/..\screens\earning\Earning.screen` | `/_sitemap` | `/bookings` | `/login` | `/profile` | `/schedule` | `/track-bus`;
+      StaticRoutes: `/` | `/(routes)/earning` | `/(routes)/login` | `/(tabs)` | `/(tabs)/` | `/(tabs)/bookings` | `/(tabs)/profile` | `/(tabs)/schedule` | `/(tabs)/track-bus` | `/_sitemap` | `/bookings` | `/earning` | `/login` | `/profile` | `/schedule` | `/track-bus`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
--- frontend/.expo/types/router.d.ts
+++ frontend/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(routes)` | `/(routes)/highway_schedules` | `/(routes)/intercity_schedules` | `/(routes)/onboarding` | `/(routes)/privatebus_schedules` | `/(routes)/schedules` | `/(routes)/sltb_schedules` | `/(tabs)` | `/(tabs)/` | `/(tabs)/notification` | `/(tabs)/profile` | `/(tabs)/tickets` | `/(tabs)/track-bus` | `/_sitemap` | `/highway_schedules` | `/intercity_schedules` | `/notification` | `/onboarding` | `/privatebus_schedules` | `/profile` | `/schedules` | `/sltb_schedules` | `/tickets` | `/track-bus`;
+      StaticRoutes: `/` | `/(routes)/highway_schedules` | `/(routes)/intercity_schedules` | `/(routes)/onboarding` | `/(routes)/privatebus_schedules` | `/(routes)/schedules` | `/(routes)/sltb_schedules` | `/(tabs)` | `/(tabs)/` | `/(tabs)/notification` | `/(tabs)/profile` | `/(tabs)/tickets` | `/(tabs)/track-bus` | `/_sitemap` | `/highway_schedules` | `/intercity_schedules` | `/notification` | `/onboarding` | `/privatebus_schedules` | `/profile` | `/schedules` | `/sltb_schedules` | `/tickets` | `/track-bus`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
