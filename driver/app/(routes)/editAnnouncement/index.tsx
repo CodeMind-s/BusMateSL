06a98f5c82a5ceeb3e88e1221101fda6673c40f1
Muhammed Arshaq <mjmarshaq@gmail.com>
2024-10-12 00:30:53 +0530

    editAnnouncement Screen initiated

---
+++ driver/app/(routes)/editAnnouncement/index.tsx
@@ -0,0 +1,10 @@
+import EditAnnouncementScreen from '@/screens/editAnnouncement/EditAnnouncement.screen'
+import React from 'react'
+
+const EditAnnouncement = () => {
+  return (
+    <EditAnnouncementScreen />
+  )
+}
+
+export default EditAnnouncement
