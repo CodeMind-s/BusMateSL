5cc9e29f45615ebbfa5b4c708a0ae7bf0eb3fb1d
Muhammed Arshaq <mjmarshaq@gmail.com>
2024-10-12 00:31:20 +0530

    viewBooking initiated

---
+++ driver/app/(routes)/viewBooking/index.tsx
@@ -0,0 +1,10 @@
+import ViewBookingScreen from '@/screens/viewBooking/ViewBooking.screen'
+import React from 'react'
+
+const ViewBooking = () => {
+    return (
+        <ViewBookingScreen />
+    )
+}
+
+export default ViewBooking
