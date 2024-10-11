c383051364b3615d4330955a6f21a4429d2d67e5
Muhammed Arshaq <mjmarshaq@gmail.com>
2024-10-12 00:33:06 +0530

    Refactor DriverDashboardScreen component to include camera permissions and scanner functionality

--- driver/screens/driverDashboard/DriverDashboard.screen.tsx
+++ driver/screens/driverDashboard/DriverDashboard.screen.tsx
@@ -1,8 +1,10 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import ScheduleListCardComponent from '../../components/scheduleListCardComponent/scheduleListCardComponent';
-import { router } from 'expo-router';
+import { Link, router } from 'expo-router';
 import { get } from '@/helpers/api';
+import { useCameraPermissions } from 'expo-camera';
+import { Ionicons } from '@expo/vector-icons';
 
 
 interface BusProps {
@@ -13,20 +15,23 @@ interface BusProps {
 }
 
 const DriverDashboardScreen = () => {
-    const [greeting, setGreeting] = useState("Good Morning");
-    const [currentBus, setCurrentBus] = useState<BusProps>();
+  const [permission, requestPermission] = useCameraPermissions();
+  const isPermissionGranted = Boolean(permission?.granted);
 
-    useEffect(() => {
-      const fetchBus = async () => {
-          try {
-              const response = await get(`buses/profile`);
-              setCurrentBus(response.data as BusProps);
-          } catch (error) {
-              console.error("Error fetching bus profile:", error);
-          }
-        };
-        fetchBus();
-    }, []);
+  const [greeting, setGreeting] = useState("Good Morning");
+  const [currentBus, setCurrentBus] = useState<BusProps>();
+
+  useEffect(() => {
+    const fetchBus = async () => {
+      try {
+        const response = await get(`buses/profile`);
+        setCurrentBus(response.data as BusProps);
+      } catch (error) {
+        console.error("Error fetching bus profile:", error);
+      }
+    };
+    fetchBus();
+  }, []);
 
   useEffect(() => {
     const currentHour = new Date().getHours();
@@ -41,48 +46,69 @@ const DriverDashboardScreen = () => {
 
   return (
     <View className=' p-5'>
-        <View className="flex flex-row">
-            <Image 
-                source={{ uri: 'https://t4.ftcdn.net/jpg/02/18/58/51/360_F_218585163_hKijGOfFIkC3Fuo9JgX2sVGv69UKoXmM.jpg' }} 
-                style={{ width: 50, height: 50 }} 
-                className="rounded-full mr-3"
-                />
-            <View>
-                <Text className=" text-[16px] " style={{ fontWeight: "900" }}>{greeting}</Text>
-                <Text className=" text-[20px] font-extrabold text-primary " style={{ fontWeight: "900" }}>
-                    {currentBus?.busName}
-                </Text>
-            </View>
+      <View className="flex flex-row">
+        <Image
+          source={{ uri: 'https://t4.ftcdn.net/jpg/02/18/58/51/360_F_218585163_hKijGOfFIkC3Fuo9JgX2sVGv69UKoXmM.jpg' }}
+          style={{ width: 50, height: 50 }}
+          className="rounded-full mr-3"
+        />
+        <View>
+          <Text className=" text-[16px] " style={{ fontWeight: "900" }}>{greeting}</Text>
+          <Text className=" text-[20px] font-extrabold text-primary " style={{ fontWeight: "900" }}>
+            {currentBus?.busName}
+          </Text>
+        </View>
       </View>
 
       <TouchableOpacity
         onPress={() => router.push("/(tabs)/track-bus")}
         className=" w-full py-5 bg-primary rounded-xl mt-6"
       >
-        <Text className=" text-center text-[20px] font-semibold text-white">
-          Switch On Location
-        </Text>
+        <View className="flex flex-row justify-center gap-2 items-center">
+          <Ionicons name="location" size={21} color="white" />
+          <Text className=" text-center text-lg font-bold text-white">
+            Switch On Location
+          </Text>
+        </View>
       </TouchableOpacity>
 
-      <TouchableOpacity className=" w-full py-5 bg-primary rounded-xl mt-6">
-        <Text className=" text-center text-[20px] font-semibold text-white">
-          Scan Ticket
-        </Text>
-      </TouchableOpacity>
+      <View className="flex flex-row items-center justify-between mt-4 ">
+        <Pressable
+          onPress={requestPermission}
+          className="bg-blue-500 py-5 w-[48%] px-4 rounded-lg"
+        >
+          <Text className="text-white font-bold text-center">Request Permissions</Text>
+        </Pressable>
+
+        <Link href="/scanner" asChild>
+          <Pressable
+            disabled={!isPermissionGranted}
+            className={`py-5 px-4 w-[48%] rounded-lg p ${isPermissionGranted ? 'bg-green-500' : 'bg-gray-300'}`}
+          >
+            <View className="flex flex-row items-center justify-center gap-2">
+              <Ionicons name="scan" size={21} color="white" />
+              <Text
+                className={`font-bold text-lg text-center ${isPermissionGranted ? 'text-white' : 'text-gray-500'}`}
+              >
+                Scan Code
+              </Text>
+            </View>
+          </Pressable>
+        </Link>
+      </View>
+
 
       <View className=" flex flex-row justify-between items-center">
-        <TouchableOpacity className=" w-[47%] py-5 bg-primary rounded-xl mt-6">
-          <Text className=" text-center text-[18px] font-semibold text-white">
-            Booked Seats
-          </Text>
-        </TouchableOpacity>
         <TouchableOpacity
           onPress={() => router.push("/(routes)/announcement")}
-          className=" w-[47%] py-5 bg-primary rounded-xl mt-6"
+          className=" w-[100%] py-5 bg-primary rounded-xl mt-6"
         >
-          <Text className=" text-center text-[18px] font-semibold text-white">
-            Announcement
-          </Text>
+          <View className="flex flex-row justify-center items-center gap-2">
+            <Ionicons name="chatbubbles" size={21} color="white" />
+            <Text className=" text-center text-lg font-bold text-white">
+              Announcement
+            </Text>
+          </View>
         </TouchableOpacity>
       </View>
 
@@ -119,5 +145,7 @@ const DriverDashboardScreen = () => {
     </View>
   );
 };
+
+
 
 export default DriverDashboardScreen;
